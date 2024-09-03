ad0db8631e9d45a150c0faeff152548878cfd932
Eclipixie <85773134+Eclipixie@users.noreply.github.com>
2024-09-03 16:38:54 +1000

    Dependency injection for damage icons (incomplete)

---
+++ js/data.js
@@ -0,0 +1,84 @@
+// name : info:tuple(shortDesc:str, longDesc:str, factions:dict(name:mul))
+
+const damageInfo = {
+    "Damage": [
+        "No additional effect.",
+        "No additional effect.",
+        { }
+    ],
+    "Impact": [
+        "Inflicts Stagger (max 5 stacks).<br>6s crowd control.<br>Increases Parazon mercy kill threshold.",
+        "Inflicts Stagger (max 5 stacks).<br>Staggers enemies for 6s. Reapplication resets the stagger. Ospreys, Tenno and bosses are immune to stagger.<br>Increases Parazon mercy kill threshold by 8% (max HP or existing?) per stack, up to 80% max health.",
+        { }
+    ],
+    "Puncture": [
+        "Inflicts Weakened (max 5 stacks).<br>Reduces target's damage.",
+        "Inflicts Weakened (max 5 stacks). Reduces enemy damage output by 40% at one stack.\nFurther stacks decrease enemy damage output by 10% per stack, up to 80% reduced damage.",
+        { }
+    ],
+    "Slash": [
+        "Inflicts Stagger (max 5 stacks).<br>6s crowd control.<br>Increases Parazon mercy kill threshold.",
+        "Inflicts Bleed (unlimited stacks).<br>Deals damage every 6 seconds (first tick is delayed by 1 second). Each stack deals damage individually",
+        { }
+    ],
+    "Cold": [
+        "Inflicts Stagger (max 5 stacks).<br>6s crowd control.<br>Increases Parazon mercy kill threshold.",
+        "Inflicts Stagger (max 5 stacks).<br>Staggers enemies for 6s. Reapplication resets the stagger. Ospreys, Tenno and bosses are immune to stagger.<br>Increases Parazon mercy kill threshold by 8% (max HP or existing?) per stack, up to 80% max health.",
+        { }
+    ],
+    "Toxin": [
+        "Inflicts Stagger (max 5 stacks).<br>6s crowd control.<br>Increases Parazon mercy kill threshold.",
+        "Inflicts Stagger (max 5 stacks).<br>Staggers enemies for 6s. Reapplication resets the stagger. Ospreys, Tenno and bosses are immune to stagger.<br>Increases Parazon mercy kill threshold by 8% (max HP or existing?) per stack, up to 80% max health.",
+        { }
+    ],
+    "Heat": [
+        "Inflicts Stagger (max 5 stacks).<br>6s crowd control.<br>Increases Parazon mercy kill threshold.",
+        "Inflicts Stagger (max 5 stacks).<br>Staggers enemies for 6s. Reapplication resets the stagger. Ospreys, Tenno and bosses are immune to stagger.<br>Increases Parazon mercy kill threshold by 8% (max HP or existing?) per stack, up to 80% max health.",
+        { }
+    ],
+    "Electricity": [
+        "Inflicts Stagger (max 5 stacks).<br>6s crowd control.<br>Increases Parazon mercy kill threshold.",
+        "Inflicts Stagger (max 5 stacks).<br>Staggers enemies for 6s. Reapplication resets the stagger. Ospreys, Tenno and bosses are immune to stagger.<br>Increases Parazon mercy kill threshold by 8% (max HP or existing?) per stack, up to 80% max health.",
+        { }
+    ],
+    "Blast": [
+        "Inflicts Stagger (max 5 stacks).<br>6s crowd control.<br>Increases Parazon mercy kill threshold.",
+        "Inflicts Stagger (max 5 stacks).<br>Staggers enemies for 6s. Reapplication resets the stagger. Ospreys, Tenno and bosses are immune to stagger.<br>Increases Parazon mercy kill threshold by 8% (max HP or existing?) per stack, up to 80% max health.",
+        { }
+    ],
+    "Corrosive": [
+        "Inflicts Stagger (max 5 stacks).<br>6s crowd control.<br>Increases Parazon mercy kill threshold.",
+        "Inflicts Stagger (max 5 stacks).<br>Staggers enemies for 6s. Reapplication resets the stagger. Ospreys, Tenno and bosses are immune to stagger.<br>Increases Parazon mercy kill threshold by 8% (max HP or existing?) per stack, up to 80% max health.",
+        { }
+    ],
+    "Magnetic": [
+        "Inflicts Stagger (max 5 stacks).<br>6s crowd control.<br>Increases Parazon mercy kill threshold.",
+        "Inflicts Stagger (max 5 stacks).<br>Staggers enemies for 6s. Reapplication resets the stagger. Ospreys, Tenno and bosses are immune to stagger.<br>Increases Parazon mercy kill threshold by 8% (max HP or existing?) per stack, up to 80% max health.",
+        { }
+    ],
+    "Viral": [
+        "Inflicts Stagger (max 5 stacks).<br>6s crowd control.<br>Increases Parazon mercy kill threshold.",
+        "Inflicts Stagger (max 5 stacks).<br>Staggers enemies for 6s. Reapplication resets the stagger. Ospreys, Tenno and bosses are immune to stagger.<br>Increases Parazon mercy kill threshold by 8% (max HP or existing?) per stack, up to 80% max health.",
+        { }
+    ],
+    "Gas": [
+        "Inflicts Stagger (max 5 stacks).<br>6s crowd control.<br>Increases Parazon mercy kill threshold.",
+        "Inflicts Stagger (max 5 stacks).<br>Staggers enemies for 6s. Reapplication resets the stagger. Ospreys, Tenno and bosses are immune to stagger.<br>Increases Parazon mercy kill threshold by 8% (max HP or existing?) per stack, up to 80% max health.",
+        { }
+    ],
+    "Radiation": [
+        "Inflicts Stagger (max 5 stacks).<br>6s crowd control.<br>Increases Parazon mercy kill threshold.",
+        "Inflicts Stagger (max 5 stacks).<br>Staggers enemies for 6s. Reapplication resets the stagger. Ospreys, Tenno and bosses are immune to stagger.<br>Increases Parazon mercy kill threshold by 8% (max HP or existing?) per stack, up to 80% max health.",
+        { }
+    ],
+    "Void": [
+        "Inflicts Stagger (max 5 stacks).<br>6s crowd control.<br>Increases Parazon mercy kill threshold.",
+        "Inflicts Stagger (max 5 stacks).<br>Staggers enemies for 6s. Reapplication resets the stagger. Ospreys, Tenno and bosses are immune to stagger.<br>Increases Parazon mercy kill threshold by 8% (max HP or existing?) per stack, up to 80% max health.",
+        { }
+    ],
+    "True": [
+        "Inflicts Stagger (max 5 stacks).<br>6s crowd control.<br>Increases Parazon mercy kill threshold.",
+        "Inflicts Stagger (max 5 stacks).<br>Staggers enemies for 6s. Reapplication resets the stagger. Ospreys, Tenno and bosses are immune to stagger.<br>Increases Parazon mercy kill threshold by 8% (max HP or existing?) per stack, up to 80% max health.",
+        { }
+    ]
+};
--- js/templates.js
+++ js/templates.js
@@ -1,7 +1,23 @@
 let factions = ["Grineer", "Kuva Grineer", "Corpus", "Corpus Amalgam", "Infested", "Infested Deimos", "Orokin", "Sentient", "Narmer", "The Murmur", "Zariman"];
 
-function damageIcon(name, colour, imageURL, factionDamage) {
+function damageIcon(name, colour, imageURL, desc, factionDamage, subtext="") {
     let factionDamages = [];
+
+    factionDamages.push(
+        '<div class="icon-info icon-info-left" style="--info-y: -40px; height: 40px; border: 0px;"> <div class="subheading">'+subtext+'</div> </div>'
+    );
+
+    factionDamages.push(
+        '<div class="icon-info icon-info-left" style="--info-y: 0px; height: 120px"></div>'
+    );
+
+    factionDamages.push(
+        '<div class="icon-info icon-info-right" style="--info-y: 0px; height: 120px"></div>'
+    );
+
+    factionDamages.push(
+        '<div class="icon-info icon-info-left" style="--info-y: 0px; height: 120px;"> <div class="center-y small-text" style="margin-right: 70px;">'+desc+'</div></div>'
+    );
 
     let factionCounter = 0;
 
@@ -12,25 +28,28 @@ function damageIcon(name, colour, imageURL, factionDamage) {
         const damage = Number.parseFloat(factionDamage[i]).toFixed(1);
         // true is left, false is right
         lr = 'left';
-        h = (factionCounter);
+        h = factionCounter;
         col = 'white';
         if (damage < 1) { col = 'red'; }
         if (damage > 1) { col = 'green'; }
 
         factionDamages.push(
-            '<div class="icon-info icon-info-left" style="--info-y: calc(100px * 1.2 / 4 * '+h+')"> <b style="color: '+col+';">'+damage+'x</b> <div class="center-y">'+faction+'</div></div>'
+            '<div class="icon-info icon-info-right" style="--info-y: calc(100px * 1.2 / 4 * '+h+')"> <b style="color: '+col+';">'+damage+'x</b> <div class="center-y">'+faction+'</div></div>'
         );
 
         factionCounter++;
     }
 
     factionDamages.push(
-        '<a class="icon-info icon-info-left" style="--info-y: calc(100px * 1.2 / 4 * '+3+')" href="https://warframe.fandom.com/wiki/Damage/'+name+'_Damage"> <div class="center-y">Wiki</div> </a>'
+        '<a class="icon-info icon-info-right" style="--info-y: calc(100px * 1.2 / 4 * '+3+')" href="https://warframe.fandom.com/wiki/Damage/'+name+'_Damage"> <div class="center-y">Wiki</div> </a>'
     );
 
     html = "";
 
     html += '<div class="icon center-y" style="--border-col: '+colour+'"><img src="'+imageURL+'" alt="">';
+    html += '<div class="icon-info" style="--info-y: -40px; height: calc(40px + 120px); background-color: var(--col-dark);">';
+    html += '<img src="'+imageURL+'" class="center-x" style="max-width: 70%; opacity: 0.3;">';
+    html += '</div>';
     html += '<div class="icon-info" style="--info-y: -40px"><h2 class="center-x center-y">'+name+'</h2></div>';
     for (let i = 0; i < factionDamages.length; i++) {
         const element = factionDamages[i];
@@ -42,37 +61,53 @@ function damageIcon(name, colour, imageURL, factionDamage) {
 }
 
 $("#basic-damage").append(damageIcon("Damage", "rgba(41,50,57,255)", "resources/DamageIcon.png", 
+    damageInfo["Damage"][0],
     [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]));
 $("#basic-damage").append(damageIcon("Impact", "rgba(52,66,66,255)", "resources/ImpactIcon.png", 
+    damageInfo["Impact"][0],
     [1.5, 1.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]));
 $("#basic-damage").append(damageIcon("Puncture", "rgba(53,47,41,255)", "resources/PunctureIcon.png", 
+    damageInfo["Puncture"][0],
     [1.0, 1.0, 1.5, 1.0, 1.0, 1.0, 1.5, 1.0, 1.0, 1.0, 1.0]));
 $("#basic-damage").append(damageIcon("Slash", "rgba(56,42,43,255)", "resources/SlashIcon.png", 
+    damageInfo["Slash"][0], // needs calculator
     [1.0, 1.0, 1.0, 1.0, 1.5, 1.0, 1.0, 1.0, 1.5, 1.0, 1.0]));
 
 $("#elemental-damage").append(damageIcon("Cold", "rgba(49,102,230,255)", "resources/ColdIcon.png", 
+    damageInfo["Cold"][0],
     [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.5, 1.0, 1.0, 1.0]));
 $("#elemental-damage").append(damageIcon("Electricity", "rgba(73,35,112,255)", "resources/ElectricityIcon.png", 
+    damageInfo["Electricity"][0], // needs calculator
     [1.0, 1.0, 1.0, 1.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.5, 1.0]));
 $("#elemental-damage").append(damageIcon("Heat", "rgba(119,54,0,255)", "resources/HeatIcon.png", 
+    damageInfo["Heat"][0], // needs calculator
     [1.0, 0.5, 1.0, 1.0, 1.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]));
 $("#elemental-damage").append(damageIcon("Toxin", "rgba(42,76,51,255)", "resources/ToxinIcon.png", 
+    damageInfo["Toxin"][0],
     [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.5, 1.0, 1.0]));
 
 $("#combinations-one").append(damageIcon("Blast", "rgba(84,29,32,255)", "resources/BlastIcon.png", 
-    [1.0, 1.0, 1.0, 0.5, 1.0, 1.5, 1.0, 1.0, 1.0, 1.0, 1.0]));
+    damageInfo["Blast"][0], // needs calculator
+    [1.0, 1.0, 1.0, 0.5, 1.0, 1.5, 1.0, 1.0, 1.0, 1.0, 1.0], "Heat + Cold"));
 $("#combinations-one").append(damageIcon("Corrosive", "rgba(49,64,7,255)", "resources/CorrosiveIcon.png", 
-    [1.5, 1.5, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5, 1.0, 1.0, 1.0]));
+    damageInfo["Corrosive"][0],
+    [1.5, 1.5, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5, 1.0, 1.0, 1.0], "Toxin + Electricity"));
 $("#combinations-one").append(damageIcon("Gas", "rgba(30,58,44,255)", "resources/GasIcon.png", 
-    [1.0, 1.0, 1.0, 1.0, 1.0, 1.5, 1.0, 1.0, 1.0, 1.0, 1.0]));
+    damageInfo["Gas"][0],
+    [1.0, 1.0, 1.0, 1.0, 1.0, 1.5, 1.0, 1.0, 1.0, 1.0, 1.0], "Toxin + Heat"));
 $("#combinations-two").append(damageIcon("Magnetic", "rgba(35,43,45,255)", "resources/MagneticIcon.png", 
-    [1.0, 1.0, 1.5, 1.5, 1.0, 1.0, 1.0, 1.0, 0.5, 1.0, 1.0]));
+    damageInfo["Magnetic"][0],
+    [1.0, 1.0, 1.5, 1.5, 1.0, 1.0, 1.0, 1.0, 0.5, 1.0, 1.0], "Cold + Electricity"));
 $("#combinations-two").append(damageIcon("Radiation", "rgba(152,74,0,255)", "resources/RadiationIcon.png", 
-    [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5, 1.5, 1.0, 1.5, 1.0]));
+    damageInfo["Radiation"][0],
+    [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5, 1.5, 1.0, 1.5, 1.0], "Heat + Electricity"));
 $("#combinations-two").append(damageIcon("Viral", "rgba(173,69,106,255)", "resources/ViralIcon.png", 
-    [1.0, 1.0, 1.0, 1.0, 1.0, 0.5, 1.5, 1.0, 1.0, 0.5, 1.0]));
+    damageInfo["Viral"][0],
+    [1.0, 1.0, 1.0, 1.0, 1.0, 0.5, 1.5, 1.0, 1.0, 0.5, 1.0], "Cold + Toxin"));
     
 $("#special-damage").append(damageIcon("Void", "rgba(6,104,108,255)", "resources/VoidIcon.png", 
+    damageInfo["Void"][0],
     [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.5]));
 $("#special-damage").append(damageIcon("True", "rgba(86,68,30,255)", "resources/TrueIcon.png", 
+    damageInfo["True"][0],
     [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]));
