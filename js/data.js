b16c2d4fba2886425976f0e33a7bf779f6fbb396
Eclipixie <85773134+Eclipixie@users.noreply.github.com>
2024-09-04 21:07:22 +1000

    Completing short form damage descriptions, index

--- js/data.js
+++ js/data.js
@@ -7,78 +7,78 @@ const damageInfo = {
         { }
     ],
     "Impact": [
-        "Inflicts Stagger (max 5 stacks).<br>6s crowd control.<br>Increases Parazon mercy kill threshold.",
+        "Inflicts Stagger (<5 stacks).<br>6s crowd control.<br>Increases Parazon mercy kill threshold.",
         "Inflicts Stagger (max 5 stacks).<br>Staggers enemies for 6s. Reapplication resets the stagger. Ospreys, Tenno and bosses are immune to stagger.<br>Increases Parazon mercy kill threshold by 8% (max HP or existing?) per stack, up to 80% max health.",
         { }
     ],
     "Puncture": [
-        "Inflicts Weakened (max 5 stacks).<br>Reduces target's damage.",
+        "Inflicts Weakened (<5 stacks).<br>Reduces target's damage.",
         "Inflicts Weakened (max 5 stacks). Reduces enemy damage output by 40% at one stack.\nFurther stacks decrease enemy damage output by 10% per stack, up to 80% reduced damage.",
         { }
     ],
     "Slash": [
-        "Inflicts Stagger (max 5 stacks).<br>6s crowd control.<br>Increases Parazon mercy kill threshold.",
+        "Inflicts Bleed (unlimited stacks).<br>DoT every 6 seconds for every stack individually.",
         "Inflicts Bleed (unlimited stacks).<br>Deals damage every 6 seconds (first tick is delayed by 1 second). Each stack deals damage individually",
         { }
     ],
     "Cold": [
-        "Inflicts Stagger (max 5 stacks).<br>6s crowd control.<br>Increases Parazon mercy kill threshold.",
+        "Inflicts Freeze (<10 stacks).<br>Reduces target's animation speed.<br>10th stack freezes target.",
         "Inflicts Stagger (max 5 stacks).<br>Staggers enemies for 6s. Reapplication resets the stagger. Ospreys, Tenno and bosses are immune to stagger.<br>Increases Parazon mercy kill threshold by 8% (max HP or existing?) per stack, up to 80% max health.",
         { }
     ],
     "Toxin": [
-        "Inflicts Stagger (max 5 stacks).<br>6s crowd control.<br>Increases Parazon mercy kill threshold.",
+        "Inflicts Poison (unlimited stacks).<br>DoT directly to health every 6 seconds for every stack individually.",
         "Inflicts Stagger (max 5 stacks).<br>Staggers enemies for 6s. Reapplication resets the stagger. Ospreys, Tenno and bosses are immune to stagger.<br>Increases Parazon mercy kill threshold by 8% (max HP or existing?) per stack, up to 80% max health.",
         { }
     ],
     "Heat": [
-        "Inflicts Stagger (max 5 stacks).<br>6s crowd control.<br>Increases Parazon mercy kill threshold.",
+        "Inflicts Ignite (unlimited stacks).<br>Reduces target's armour.<br>DoT every 6 seconds for every stack individually.",
         "Inflicts Stagger (max 5 stacks).<br>Staggers enemies for 6s. Reapplication resets the stagger. Ospreys, Tenno and bosses are immune to stagger.<br>Increases Parazon mercy kill threshold by 8% (max HP or existing?) per stack, up to 80% max health.",
         { }
     ],
     "Electricity": [
-        "Inflicts Stagger (max 5 stacks).<br>6s crowd control.<br>Increases Parazon mercy kill threshold.",
+        "Inflicts Tesla Chain (<1 stack).<br>3m AoE DoT centered on target every 6 seconds.",
         "Inflicts Stagger (max 5 stacks).<br>Staggers enemies for 6s. Reapplication resets the stagger. Ospreys, Tenno and bosses are immune to stagger.<br>Increases Parazon mercy kill threshold by 8% (max HP or existing?) per stack, up to 80% max health.",
         { }
     ],
     "Blast": [
-        "Inflicts Stagger (max 5 stacks).<br>6s crowd control.<br>Increases Parazon mercy kill threshold.",
+        "Inflicts Detonate (<10 stacks).<br>5m AoE after 1.5s, when stacks are maxxed, or target dies.",
         "Inflicts Stagger (max 5 stacks).<br>Staggers enemies for 6s. Reapplication resets the stagger. Ospreys, Tenno and bosses are immune to stagger.<br>Increases Parazon mercy kill threshold by 8% (max HP or existing?) per stack, up to 80% max health.",
         { }
     ],
     "Corrosive": [
-        "Inflicts Stagger (max 5 stacks).<br>6s crowd control.<br>Increases Parazon mercy kill threshold.",
+        "Inflicts Corrosion (<10 stacks).<br>Reduces target's armour.",
         "Inflicts Stagger (max 5 stacks).<br>Staggers enemies for 6s. Reapplication resets the stagger. Ospreys, Tenno and bosses are immune to stagger.<br>Increases Parazon mercy kill threshold by 8% (max HP or existing?) per stack, up to 80% max health.",
         { }
     ],
     "Magnetic": [
-        "Inflicts Stagger (max 5 stacks).<br>6s crowd control.<br>Increases Parazon mercy kill threshold.",
+        "Inflicts Disrupt (<10 stacks).<br>Increases damage to target's shield and overguard. Shield breaks deal Electricity.",
         "Inflicts Stagger (max 5 stacks).<br>Staggers enemies for 6s. Reapplication resets the stagger. Ospreys, Tenno and bosses are immune to stagger.<br>Increases Parazon mercy kill threshold by 8% (max HP or existing?) per stack, up to 80% max health.",
         { }
     ],
     "Viral": [
-        "Inflicts Stagger (max 5 stacks).<br>6s crowd control.<br>Increases Parazon mercy kill threshold.",
+        "Inflicts Virus (<5 stacks).<br>Increases damage to target's health.",
         "Inflicts Stagger (max 5 stacks).<br>Staggers enemies for 6s. Reapplication resets the stagger. Ospreys, Tenno and bosses are immune to stagger.<br>Increases Parazon mercy kill threshold by 8% (max HP or existing?) per stack, up to 80% max health.",
         { }
     ],
     "Gas": [
-        "Inflicts Stagger (max 5 stacks).<br>6s crowd control.<br>Increases Parazon mercy kill threshold.",
+        "Inflicts Gas Cloud (<10 stack).<br>3m+(0.3*x)m AoE DoT centered on target at time of application.",
         "Inflicts Stagger (max 5 stacks).<br>Staggers enemies for 6s. Reapplication resets the stagger. Ospreys, Tenno and bosses are immune to stagger.<br>Increases Parazon mercy kill threshold by 8% (max HP or existing?) per stack, up to 80% max health.",
         { }
     ],
     "Radiation": [
-        "Inflicts Stagger (max 5 stacks).<br>6s crowd control.<br>Increases Parazon mercy kill threshold.",
+        "Inflicts Confusion (<1 stack).<br>Targets cannot tell friend from foe.",
         "Inflicts Stagger (max 5 stacks).<br>Staggers enemies for 6s. Reapplication resets the stagger. Ospreys, Tenno and bosses are immune to stagger.<br>Increases Parazon mercy kill threshold by 8% (max HP or existing?) per stack, up to 80% max health.",
         { }
     ],
     "Void": [
-        "Inflicts Stagger (max 5 stacks).<br>6s crowd control.<br>Increases Parazon mercy kill threshold.",
+        "Inflicts Bullet Attractor (<1 stack).<br>Attracts bullets in a 2.5m radius centered on target at time of application.",
         "Inflicts Stagger (max 5 stacks).<br>Staggers enemies for 6s. Reapplication resets the stagger. Ospreys, Tenno and bosses are immune to stagger.<br>Increases Parazon mercy kill threshold by 8% (max HP or existing?) per stack, up to 80% max health.",
         { }
     ],
     "True": [
-        "Inflicts Stagger (max 5 stacks).<br>6s crowd control.<br>Increases Parazon mercy kill threshold.",
-        "Inflicts Stagger (max 5 stacks).<br>Staggers enemies for 6s. Reapplication resets the stagger. Ospreys, Tenno and bosses are immune to stagger.<br>Increases Parazon mercy kill threshold by 8% (max HP or existing?) per stack, up to 80% max health.",
+        "Is unaffected by all forms of damage reduction and vulnerability. Bypasses shields, but not overguard.",
+        "Is unaffected by all forms of damage reduction and vulnerability. Bypasses shields, but not overguard.",
         { }
     ]
 };
